c7be0345c020f4fb1a51e628bdec4ba237221337
Hung Pham <tienhung278@gmail.com>
2023-07-22 11:51:20 +0800

    Register dependencies to IoC

--- Service1/Program.cs
+++ Service1/Program.cs
@@ -1,25 +1,44 @@
+using System.Reflection;
+using Service1.Extensions;
+using Service1.Models;
+using Service1.Repositories;
+using Service1.Repositories.Contracts;
+using Service1.Services;
+using Service1.Services.Contracts;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    builder.Services.Configure<DataStore>(builder.Configuration.GetSection("DataStore"));
+    builder.Services.AddScoped<RepositoryContext>();
+    builder.Services.AddScoped<IRepositoryManager, RepositoryManager>();
+    builder.Services.AddScoped<IServiceManager, ServiceManager>();
+    builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+    builder.Services.AddCors(opt =>
+    {
+        opt.AddDefaultPolicy(policyBuilder =>
+        {
+            policyBuilder.AllowAnyMethod();
+            policyBuilder.AllowAnyHeader();
+            policyBuilder.AllowAnyOrigin();
+        });
+    });
 }
 
-app.UseHttpsRedirection();
+var app = builder.Build();
+{
+    app.UseGlobalException();
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
 
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+    app.UseHttpsRedirection();
+    app.UseAuthorization();
+    app.UseCors();
+    app.MapControllers();
+    app.Run();
+}
