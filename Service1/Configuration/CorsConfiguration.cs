fc762e3d3ae1cb87606e5b3201c5383ff30183ae
Hung Pham <tienhung278@gmail.com>
2023-07-22 20:10:36 +0800

    Create seri logger and cors configuration

---
+++ Service1/Configuration/CorsConfiguration.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Cors.Infrastructure;
+
+namespace Service1.Configuration;
+
+public static class CorsConfiguration
+{
+    public static Action<CorsOptions> Configure => (options =>
+    {
+        options.AddDefaultPolicy(policyBuilder =>
+        {
+            policyBuilder.AllowAnyMethod();
+            policyBuilder.AllowAnyHeader();
+            policyBuilder.AllowAnyOrigin();
+        });
+    });
+}
--- Service1/Program.cs
+++ Service1/Program.cs
@@ -1,4 +1,6 @@
 using System.Reflection;
+using Serilog;
+using Service1.Configuration;
 using Service1.Extensions;
 using Service1.Models;
 using Service1.Repositories;
@@ -16,15 +18,8 @@ var builder = WebApplication.CreateBuilder(args);
     builder.Services.AddScoped<IRepositoryManager, RepositoryManager>();
     builder.Services.AddScoped<IServiceManager, ServiceManager>();
     builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
-    builder.Services.AddCors(opt =>
-    {
-        opt.AddDefaultPolicy(policyBuilder =>
-        {
-            policyBuilder.AllowAnyMethod();
-            policyBuilder.AllowAnyHeader();
-            policyBuilder.AllowAnyOrigin();
-        });
-    });
+    builder.Services.AddCors(CorsConfiguration.Configure);
+    builder.Host.UseSerilog(SeriLoggerConfiguration.Configure);
 }
 
 var app = builder.Build();
