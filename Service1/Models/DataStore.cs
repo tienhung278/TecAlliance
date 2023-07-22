b95df3418479ac4a5ffef019a32fbd18b688c710
Hung Pham <tienhung278@gmail.com>
2023-07-22 09:13:24 +0800

    Create repository context

---
+++ Service1/Models/DataStore.cs
@@ -0,0 +1,6 @@
+namespace Service1.Models;
+
+public class DataStore
+{
+    public string? FolderPath { get; set; }
+}
---
+++ Service1/Repositories/RepositoryContext.cs
@@ -0,0 +1,54 @@
+using System.Text.Json;
+using Microsoft.Extensions.Options;
+using Service1.Models;
+
+namespace Service1.Repositories;
+
+public class RepositoryContext
+{
+    private object? _store;
+    private string? _fileName;
+    private readonly string? _folder;
+    private readonly Dictionary<string, object?> _stores;
+
+    public RepositoryContext(IOptions<DataStore> configuration)
+    {
+        _stores = new Dictionary<string, object?>();
+        var dataStore = configuration.Value;
+        _folder = dataStore?.FolderPath;
+    }
+
+    public List<T>? Set<T>() where T : class
+    {
+        _fileName = $"{_folder}{typeof(T).Name}Store.json";
+        if (File.Exists(_fileName))
+        {
+            var json = File.ReadAllText(_fileName);
+            _store = JsonSerializer.Deserialize<List<T>>(json);
+        }
+        else
+        {
+            _store = new List<T>();
+        }
+        
+        _stores.Add(_fileName, _store);
+
+        return _store as List<T>;
+    }
+
+    public void SaveChanges()
+    {
+        foreach (var store in _stores)
+        {
+            var fileName = store.Key;
+            var s = store.Value;
+            
+            var json = JsonSerializer.Serialize(_store);
+        
+            if (_fileName != null)
+            {
+                File.WriteAllText(_fileName, json);
+            }
+        }
+    }
+}
