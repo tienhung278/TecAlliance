b1fa162e1cd544dbc4a2d3713d2ceafa23d71753
Hung Pham <tienhung278@gmail.com>
2023-07-24 07:33:30 +0800

    Implement employee cache

--- Service1.APP/src/app/modules/shared/services/employee/employee.service.ts
+++ Service1.APP/src/app/modules/shared/services/employee/employee.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {BehaviorSubject, Observable, tap} from "rxjs";
 import {Employee} from "../../../../models/employee";
+import {CacheService} from "../cache/cache.service";
+import {CACHE_KEYS} from "../../../../models/cache-keys";
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +12,16 @@ export class EmployeeService {
 
   private baseUrl: string = "http://localhost:5141/api";
 
-  constructor(private client: HttpClient) { }
+  constructor(private client: HttpClient, private cache: CacheService) { }
 
   public getEmployees(): Observable<Employee[]> {
-    return this.client.get<Employee[]>(this.baseUrl + "/employees");
+    const employees = this.cache.getData(CACHE_KEYS.EMPLOYEES);
+    if (employees) {
+      return new BehaviorSubject<Employee[]>(JSON.parse(employees));
+    }
+    return this.client.get<Employee[]>(this.baseUrl + "/employees").pipe(
+      tap(v => this.cache.saveData(CACHE_KEYS.EMPLOYEES, JSON.stringify(v)))
+    );
   }
 
   public getEmployee(id: string): Observable<Employee> {
@@ -21,14 +29,17 @@ export class EmployeeService {
   }
 
   public createEmployee(body: any): Observable<Employee> {
+    this.cache.removeData(CACHE_KEYS.EMPLOYEES);
     return this.client.post<Employee>(this.baseUrl + "/employees", body, { headers: { "Content-Type": "application/json" }});
   }
 
   public updateEmployee(id: string, body: any): Observable<any> {
+    this.cache.removeData(CACHE_KEYS.EMPLOYEES);
     return this.client.put<any>(this.baseUrl + "/employees/" + id, body, { headers: { "Content-Type": "application/json" }});
   }
 
   public deleteEmployee(id: string): Observable<any> {
+    this.cache.removeData(CACHE_KEYS.EMPLOYEES);
     return this.client.delete(this.baseUrl + "/employees/" + id);
   }
 }
